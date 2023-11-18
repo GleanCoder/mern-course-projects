15185bddf7b5981ad3135402379b71d1093290be
GleanCoder <adityakiranacharya0@gmail.com>
2023-11-18 19:02:31 +0530

    javascript folder added

---
+++ javascript/course-pratices/helloworld/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let number1 = 10;
+let number2 = 5;
+let sumNumber = number1 + number2;
+
+if (sumNumber === 15) {
+  alert("Hey! Welcome to Javascript");
+} else {
+  alert("OPPS! Something Wrong");
+}
