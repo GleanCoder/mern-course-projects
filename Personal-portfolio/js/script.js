d3cf27abe38a5648ae38a45df57f7c6f09f69867
GleanCoder <adityakiranacharya0@gmail.com>
2023-11-20 18:19:13 +0530

    queries.css updated

--- Personal-portfolio/js/script.js
+++ Personal-portfolio/js/script.js
@@ -1,4 +1,4 @@
-var typeData = new Typed(".role", {
+let typeData = new Typed(".role", {
   strings: [
     "Web Developer",
     "Front-End Developer",
@@ -10,6 +10,25 @@ var typeData = new Typed(".role", {
   backSpeed: 80,
   backDelay: 1000,
 });
+
+function adjustLayout() {
+  var screenWidth = window.innerWidth;
+  var prefixElement = document.querySelector(".prefix");
+  var roleElement = document.querySelector(".role");
+
+  if (screenWidth < 544) {
+    prefixElement.innerHTML = "I am a<br>";
+
+    roleElement.style.marginTop = "1.2rem";
+  } else {
+    prefixElement.innerHTML = "I am a";
+    roleElement.style.marginTop = "0";
+  }
+}
+
+// Call the function on page load and window resize
+document.addEventListener("DOMContentLoaded", adjustLayout);
+window.addEventListener("resize", adjustLayout);
 
 // mobile navigation work
 const navButtonEl = document.querySelector(".mobile-nav-btn");
