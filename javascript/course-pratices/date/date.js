9127d6189597fa8a8cf9ab773fa8977313962709
GleanCoder <adityakiranacharya0@gmail.com>
2023-12-09 23:59:01 +0530

    date folder is added

---
+++ javascript/course-pratices/date/date.js
@@ -0,0 +1,12 @@
+let currentTime = new Date();
+console.log(currentTime);
+
+currentTime = new Date(2023, 11, 10, 11, 50, 0, 0);
+console.log(currentTime);
+
+// const year = curreDate.getFullYear();
+// console.log(year);
+
+// const timestamp = currentDate.getTime();
+
+// console.log(timestamp);
